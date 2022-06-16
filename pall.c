ff340433df9c3fc45357852f932cf7c25c0b34a5
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-16 05:37:30 -0700

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,13 +1,29 @@
 #include "monty.h"
 
 /**
- * pall - prints the stack
- * @stack: stack given by main in start.c
- * @line_cnt: amount of lines
- *
- * Return: void
- */
-void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+  * lifo - changes the addition to the stack to LIFO
+  * @stack: NULL
+  * @line_number: 0
+  * Return: nothing, void
+  */
+void lifo(stack_t **stack, unsigned int line_number)
 {
-	print_stack(*stack);
+	(void) stack;
+	(void) line_number;
+
+	element->IFO = 0;
+}
+
+/**
+  * fifo - changes the addition to the stack to FIFO
+  * @stack: NULL
+  * @line_number: 0
+  * Return: nothing, void
+  */
+void fifo(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	element->IFO = 1;
 }
