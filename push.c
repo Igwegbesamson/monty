22273f54c3dc218f7c5541c5c0fead37a3839198
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-16 05:36:52 -0700

    Update push.c

--- push.c
+++ push.c
@@ -1,38 +1,87 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * push - push element into the stack
- * @stack: stack given by main
- * @line_cnt: amount of lines
- *
+ * exit_function - handles errors and frees memory before exiting
+ * @err_num: number corresponding to an error message
  * Return: void
  */
-void push(stack_t **stack, unsigned int line_cnt)
+void exit_function(unsigned int err_num)
 {
-	char *n = global.argument;
+	char *s[] = {"swap", "add", "sub", "div", "mul", "mod"};
+	int ln = element->line_number;
 
-	if ((is_digit(n)) == 0)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-		exit(EXIT_FAILURE);
-	}
+	if (err_num == 1)
+		fprintf(stderr, "Error: Can't open file %s\n", element->fname);
+	if (err_num == 2)
+		fprintf(stderr, "L%d: unknown instruction %s\n", ln, element->tokened[0]);
+	if (err_num == 3)
+		fprintf(stderr, "Error: malloc failed\n");
+	if (err_num == 4)
+		fprintf(stderr, "L%d: usage: push integer\n", ln);
+	if (err_num == 5)
+		fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
+	if (err_num == 6)
+		fprintf(stderr, "L%d: can't pop an empty stack\n", ln);
+	if (err_num >= 7 && err_num <= 12)
+		fprintf(stderr, "L%d: can't %s, stack too short\n", ln, s[err_num - 7]);
+	if (err_num == 13)
+		fprintf(stderr, "L%d: division by zero\n", ln);
+	if (err_num == 14)
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", ln);
+	if (err_num == 15)
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", ln);
+	if (err_num == 16)
+		fprintf(stderr, "USAGE: monty file\n");
 
-	if (global.data_struct == 1)
+	free_buffer();
+	free_list(element->head);
+	free_token();
+	if (element->fp != NULL)
+		fclose(element->fp);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * free_list - free a stack_t list
+ * @head: head of list to free
+ * Return: void
+ */
+void free_list(stack_t *head)
+{
+	if (head != NULL)
 	{
-		if (!add_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
+		if (head->next != NULL)
+			free_list(head->next);
+		free(head);
 	}
-	else
+}
+
+/**
+ * free_buffer - free getline buffer
+ * Return: void
+ */
+void free_buffer(void)
+{
+	if (element->buf != NULL)
 	{
-		if (!queue_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
+		free(element->buf);
+		element->buf = NULL;
 	}
+}
+
+/**
+  * free_token - Frees malloc'd tokens
+  * Return: Nothing, void
+  */
+void free_token(void)
+{
+	if (element->tokened != NULL)
+	{
+		if (element->tokened[0] != NULL)
+			free(element->tokened[0]);
+		if (element->tokened[1] != NULL)
+			free(element->tokened[1]);
+		free(element->tokened);
+	}
+	element->tokened = NULL;
 }
